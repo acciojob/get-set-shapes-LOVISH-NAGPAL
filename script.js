ed8e49f2456a9083e00a69e49ceda8780390bd2d
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-09-18 00:31:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+construtor(width,height){
+this.width=width
+	this.height=height
+}
+	set width(width){
+		this._width=width
+	}
+	set height(height){
+		this._height=height
+	}
+	getArea(){
+		return this._width*this._height
+	}
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+
+constructor(side){
+	super(side,side)
+}
+	getPerimeter(){
+
+		return 4*this._width
+	}
+	
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
